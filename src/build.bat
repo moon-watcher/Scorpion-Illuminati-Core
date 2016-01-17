f37e4b77f604442c4c6a1da77aa012761451b047
Scorpion Illuminati <webmaster@scorpionilluminati.net>
2016-01-16 23:06:38 -0500

    Added comments, check to ensure rom file exists before deletion for clean build. command to fix rom header after build and assembler optimisation to build script

--- src/build.bat
+++ src/build.bat
@@ -1,3 +1,18 @@
-del "../bin/rom.bin"
-asm68k.exe /p source.asm,../bin/rom.bin
+@echo off
+rem delete old assembled binary if it exists
+@echo on
+if exist "../bin/rom.bin" del "../bin/rom.bin"
+
+@echo off
+rem /k enable if assembler directives
+rem /p pure binary
+rem /o enable optimisations
+rem ow+ optimise absolute long addressing
+@echo on
+asm68k.exe /k /p /o ow+ source.asm,../bin/rom.bin
+
+@echo off
+rem fix the rom header (checksum)
+@echo on
+fixheadr.exe ../bin/rom.bin
 pause
